08a78088a1af057d2a6c5c61b8acc21cee503e2e
andres l <andyguachin87@gmail.com>
2019-08-23 19:33:07 -0300

    Si la imagen no existe, eliminar sus datos de mongoDB

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const fs = require('fs');
+const path = require('path');
 const router = Router();
 const modelo = require('../models/schema');
 const coment = require('../models/coments');
@@ -9,14 +11,32 @@ router.get('/', (req, res) => {
     res.render('index');
 });
 
-router.get('/imgShow/:id', async (req, res) => {
-    var image = await modelo.findOne({ filename : req.params.id });
-    var comment = await coment.find({ id : req.params.id }).sort({_id:-1});
-    if(image) {
-        res.render('images', { image, comment })
-    } else {
-        res.send('la imagen no existe');
-    }
+router.get('/imgShow/:id', (req, res) => {
+    fs.stat(path.join(__dirname, `../public/images/${req.params.id}`), async (err, stat) => {
+        if(err == null) {
+            var image = await modelo.findOne({ filename : req.params.id });
+            var comment = await coment.find({ id : req.params.id }).sort({_id:-1});
+            if(image) {
+                res.render('images', { image, comment })
+            } else {
+                res.send('la imágen no existe');
+            }
+        } else {
+            let image2 = await modelo.findOne({ filename : req.params.id });
+            let comment2 = await coment.find({ id : req.params.id });
+            if(image2) {
+                await image2.remove();
+            }
+            
+            if(comment2) {
+                for(let i = 0; i < comment2.length; i++){
+                    comment2[i].remove();
+                }
+            }
+            
+            res.send('la imágen no existe o fue eliminada');
+        }
+    })
 }); 
 
 router.post('/imgUp', async (req, res) => {
