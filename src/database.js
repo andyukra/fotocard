d89d764ea78027f1de2c84e4a6b936a81ea11be1
andres l <andyguachin87@gmail.com>
2019-08-23 14:34:54 -0300

    he cambiado la URI de mongoDB

--- src/database.js
+++ src/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost/fotoSocial", {
+mongoose.connect("mongodb+srv://andyukra:4343@cluster0-eahfi.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser : true
 }) .then(() => console.log("db is connectd")) .catch(err => console.log(err));
 
